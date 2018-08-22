3edbaa5175657090e66c7291cb7751e528e3688e
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-08-22 01:34:06 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,8 +4,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :number_of_costumes
-      t.integer :number_of_employees
+      t.integer :costume_inventory
+      t.integer :employee_count
       t.string :in_business
       t.datetime :opening_time
       t.datetime :closing_time
